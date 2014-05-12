44e2be5aecd17791e8fcdd90c117071fde45a0a7
Gerd Riesselmann <gerd@gerd-riesselmann.net>
2014-05-12 22:10:54 +0200

    Create testcase

---
+++ src/main/java/com/strucr/grizzly1679/Main.java
@@ -0,0 +1,44 @@
+package com.strucr.grizzly1679;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
+import com.ning.http.client.Response;
+import com.ning.http.client.providers.grizzly.GrizzlyAsyncHttpProvider;
+
+import java.util.concurrent.Future;
+
+/**
+ * Run a request
+ */
+public class Main {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please pass URL to call");
+            System.exit(1);
+        }
+        String url = args[0];
+
+        AsyncHttpClientConfig config = new AsyncHttpClientConfig.Builder()
+            .setCompressionEnabled(true)
+            .setFollowRedirects(false)
+            .setUserAgent("Grizzly 1679 Test Case")
+            .setConnectionTimeoutInMs(15000)
+            .setRequestTimeoutInMs(15000)
+            .setAllowPoolingConnection(false)
+            .setUseRawUrl(true)
+            .setIOThreadMultiplier(2) // 2 is default
+            .build();
+
+        AsyncHttpClient client = new AsyncHttpClient(new GrizzlyAsyncHttpProvider(config), config);
+
+        try {
+            Future<Response> f = client.prepareGet(url).execute();
+            Response r = f.get();
+            System.out.println("Got Respone " + r.getStatusText());
+            System.exit(0);
+        } catch (Exception e) {
+            System.out.println("Cought Exception " + e.toString());
+            System.exit(2);
+        }
+    }
+}
